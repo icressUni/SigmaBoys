214220ec0926f37a785f15f3b1211657bc20f69c
Ribletx <141599625+Ribletx@users.noreply.github.com>
2025-04-08 00:46:16 -0400

    SCRUM-22 Pagina principal
    Implementación de la pagina de usuarios para visualizar la cámara que escaneara su rostro

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    const getCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error("Error al acceder a la cámara:", err);
+      }
+    };
+
+    getCamera();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Reconocimiento Facial</h1>
+      <div className="video-container">
+        <video ref={videoRef} autoPlay playsInline className="video-element" />
+      </div>
+      <button className="login-button">Manual Login</button>
+    </div>
+  );
+}
+
+export default App;
